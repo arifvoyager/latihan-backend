644fd7e2ddee625db18cf691868c78045231d1e5
arif voyager <arifvoyager02@gmail.com>
2019-09-20 11:13:39 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/filantropi/';
+$config['base_url'] = 'http://localhost/test/';
 
 /*
 |--------------------------------------------------------------------------
